1c780818a22c00af1fed19b0ba222cfb0918517b
CoonPatrol <lukemanley48@gmail.com>
2022-12-05 21:47:25 +0200

    init

---
+++ AutomatedWhatsapp.py
@@ -0,0 +1,13 @@
+import pywhatkit
+from keyboard import press
+import time
+
+
+def main():
+
+    pywhatkit.sendwhatmsg_instantly("+27711036659", "CUNT!")
+    time.sleep(1)
+    press('enter')
+
+
+main()
